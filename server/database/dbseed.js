826dd1edd5b7bc773f737431da586dbdc31ff23e
Henry Garant <henryrgarant@gmail.com>
2020-04-16 15:56:13 -0400

    connect to db

---
+++ server/database/dbseed.js
@@ -0,0 +1,20 @@
+//Sanity check for ensuring that we can connect to the database
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "cis550proj.cimxsfsxttk4.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "penn1741",
+    port: 1521,
+    database: 'cis550proj'
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(`SHOW TABLES;`, function(err, result, fields) {
+        if (err) throw (err);
+        if (result) console.log(result);
+    });
+    con.end();
+});
