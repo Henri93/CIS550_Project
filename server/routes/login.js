9ee4bf079a940850b751ffa5217a73bee9e4db32
Henri93 <henryrgarant@gmail.com>
2020-04-16 17:20:50 -0400

    backend login validation

--- server/routes/login.js
+++ server/routes/login.js
@@ -17,25 +17,25 @@ function isAuthenticated(req, res, next) {
 router.post('/', function (req, res) {
     //Attempt to login the user with credentials given
     var username = req.body.email;
-	var password = req.body.password;
+    var password = req.body.password;
     console.log("Login attempt for " + username + " with password " + password);
+    
+    db.validateLogin(username, password, function(data, err) {
+        if(data == null && err != null){
+            //send error with login
+            res.json({success: false, err: err});
+        }else{
+            //save user logged in to session
+            req.session.user = username;
+            req.session.authenticated = true;
 
-	db.validateLogin(username, password, function(data, err) {
-		if(data == null && err != null){
-			//send error with login
-			res.json({success: false, err: err});
-		}else{
-			//save user logged in to session
-			req.session.user = username;
-			req.session.authenticated = true;
-
-			//return success
-			res.json({success: true, res: data['username']});
-		}
-  });
+            //return success
+            res.json({success: true, res: data['username']});
+        }
+    });
 })
 
 module.exports = { 
     router:router,
     isAuthenticated:isAuthenticated
-  }
+}
