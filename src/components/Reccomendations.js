8d95dfde10a4ffdeed24d9d4d2afd03c05d7f0dd
jacobj9 <jplatin@micds.org>
2020-04-16 16:22:57 -0500

    added cards to reccomendation page

--- src/components/Reccomendations.js
+++ src/components/Reccomendations.js
@@ -35,15 +35,51 @@ export default class Reccomendations extends React.Component {
                 </div>
                 <div className="floater">
 
-
-                    <h1 className="bizTitle"> {this.state.username}</h1>
+                    <p className="bizTitle">{this.state.username}, based on your activity, we reccomend you check out the following people:</p>
                     <br></br>
-                    <p className="based">Based on your activity, we reccomend you check out the following people:</p>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="card" style={{ "width": "18rem" }}>
+                                    <div style = {{"height":"10vw"}} class="card-header">
+                                        <span class="nameSpanForCard">{this.state.initial}</span>
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <button type="button" class="btn btn-warning">Go to</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="card" style={{ "width": "18rem" }}>
+                                    <div style = {{"height":"10vw"}} class="card-header">
+                                        <span class="nameSpanForCard">{this.state.initial}</span>
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <button type="button" class="btn btn-warning">Go to</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="card" style={{ "width": "18rem" }}>
+                                    <div style = {{"height":"10vw"}} class="card-header">
+                                        <span class="nameSpanForCard">{this.state.initial}</span>
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <button type="button" class="btn btn-warning">Go to</button>
+                                    </div>
+                                </div>
+                            </div>
+                  
 
+                        </div>
+                    </div>
                 </div>
                 <span class="nameSpan">{this.state.initial}</span>
 
-            </div>
+            </div >
         );
     }
 }
