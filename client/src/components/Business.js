acad8e6fa9a38bdb2522cf1dceb7342672c535ee
jacobj9 <jplatin@micds.org>
2020-04-23 12:00:22 -0500

    started on GMap for location

--- client/src/components/Business.js
+++ client/src/components/Business.js
@@ -3,6 +3,18 @@ import React, { useState, useEffect } from "react";
 import PageNavbar from './Header';
 import "../style/business.css"
 import StarRatings from 'react-star-ratings';
+import {
+    withGoogleMap,
+    withScriptjs,
+    GoogleMap,
+    Marker,
+    InfoWindow
+} from "react-google-maps";
+import mapStyles from "../style/mapStyles";
+
+
+var _map;
+
 
 
 export default class Business extends React.Component {
@@ -21,50 +33,52 @@ export default class Business extends React.Component {
 
         //load businesses information
         let businessID = (this.props.location.pathname.split('/')[2]);
-        fetch('/getBusinessesInfo?id='+businessID, {
+        fetch('/getBusinessesInfo?id=' + businessID, {
             method: 'GET',
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json',
-            }})
+            }
+        })
             .then(response => response.json())
             .then(data => {
-                if(data.success) {
-                //successful
-                var bizInitial = data.business.name.toUpperCase()[0];
-                var categories = data.business.categories.replace(";"," ");
-                this.setState({
-                    business: data.business,
-                    bizInitial: bizInitial,
-                    categories: categories
-                });
+                if (data.success) {
+                    //successful
+                    var bizInitial = data.business.name.toUpperCase()[0];
+                    var categories = data.business.categories.replace(";", " ");
+                    this.setState({
+                        business: data.business,
+                        bizInitial: bizInitial,
+                        categories: categories
+                    });
 
-                
-                console.log(bizInitial);
-                }else{
+
+                    console.log(bizInitial);
+                } else {
                     //display error msg
                     console.log("Fail to load business info")
                 }
-        })
+            })
 
         //load first 10 reviews sorted by date
-        fetch('/getReviews?id='+businessID, {
+        fetch('/getReviews?id=' + businessID, {
             method: 'GET',
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json',
-            }})
+            }
+        })
             .then(response => response.json())
             .then(data => {
-                if(data.success) {
-                //successful
-                console.log("Reviews: " + data.reviews)
-                this.setState({
-                    reviews: data.reviews
-                });
-                }else{
+                if (data.success) {
+                    //successful
+                    console.log("Reviews: " + data.reviews)
+                    this.setState({
+                        reviews: data.reviews
+                    });
+                } else {
                     //display error msg
                     console.log("Fail to load reviews ofr business")
                 }
-        })
+            })
     }
 
 
@@ -77,7 +91,7 @@ export default class Business extends React.Component {
                 </div>
                 <div className="topPic">
                 </div>
-                <div className="floater">
+                <div className="bizFloater">
 
                     <h1 className="bizTitle"> {this.state.business.name}</h1>
                     <StarRatings
@@ -88,13 +102,29 @@ export default class Business extends React.Component {
                         starSpacing="2px"
                         name='rating'
                     />
-                    <p className = "numberOfReviews">{this.state.business.review_count} Reviews</p>
-                    <p className = "cats">{this.state.categories}</p>
+                    <p className="numberOfReviews">{this.state.business.review_count} Reviews</p>
+                    <p className="cats">{this.state.categories}</p>
                     <a href={"/review/" + this.state.business.name} type="button" class="reviewBut btn btn-outline-warning">Leave a review</a>
-                    <hr className = "pageBreak"></hr>
-                    <p className = "cats">{this.state.business.address}<br /> {this.state.business.city + ", " + this.state.business.state}</p>
+                    <hr className="pageBreak"></hr>
                     <h2>Location</h2>
-                    <hr className = "pageBreak"></hr>
+                    <br></br>
+                    <div class="card" style={{ "margin": "auto", "width": "25vw" }}>
+                        <div style={{ "height": "14vw" }} class="card-header">
+                            <GoogleMap
+                                ref={(map) => _map = map}
+                                defaultZoom={10}
+                                defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
+                                defaultOptions={{ styles: mapStyles }}
+                                onDragEnd={this.updateBusinesses}
+                            > </GoogleMap>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">{this.state.business.address}<br /> {this.state.business.city + ", " + this.state.business.state}</p>
+                            <a target="_blank" style={{ "backgroundColor": "orange", "color": "white" }} href="https://www.google.com/maps/dir/?api=1&destination=292+McArthur+Avenue+Vanier+ON" type="button" class="btn btn-warning">Directions</a>
+                        </div>
+                    </div>
+
+                    <hr className="pageBreak"></hr>
                     <h2>Reviews</h2>
 
 
