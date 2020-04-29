36aba331c361b3945af9b832d53e1d34e26b77c2
Henry Garant <henryrgarant@gmail.com>
2020-04-28 20:39:14 -0400

    submit review backend

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -162,12 +162,12 @@ export default function Map(props) {
     <div style={{ width: "100vw", height: "94vh" }}>
           <PageNavbar hide_search={false} active="Map" loggedInUser={props.loggedInUser} />
 
-      { <MapWrapped
+      { /*<MapWrapped
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAi8On2sh9wpXhquXfaDcdpMl_JmDbhBO0`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
-      />  }
+      />  */}
     </div>
   );
 }
