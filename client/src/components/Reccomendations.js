440596950aecc72568ee7910b98338fafeb8f6db
jacobj9 <jplatin@micds.org>
2020-05-03 19:35:47 -0500

    added new recs UI

--- client/src/components/Reccomendations.js
+++ client/src/components/Reccomendations.js
@@ -14,9 +14,16 @@ export default class Reccomendations extends React.Component {
             username: "",
             reccomendationsProfiles: [],
             mapToProfilesId: new Object,
-            mapToReason: new Object
+            mapToReason: new Object,
+            onProfile: true,
+
         };
     }
+
+    toggleVisibility = () => {
+        this.setState({ onProfile: !this.state.onProfile });
+
+    };
 
     componentDidMount() {
         this.username = this.props.loggedInUser.username;
@@ -28,29 +35,29 @@ export default class Reccomendations extends React.Component {
 
         console.log("trying to get recs for " + user_id);
 
-        fetch('/reccomendations/'+user_id, {
+        fetch('/reccomendations/' + user_id, {
             method: 'GET'
         }).then(res => {
             console.log(res);
-        // Convert the response data to a JSON.
-        return res.json();
+            // Convert the response data to a JSON.
+            return res.json();
         }, err => {
-          // Print the error if there is one.
-          console.log(err);
+            // Print the error if there is one.
+            console.log(err);
         }).then(data => {
-          if (!data) {return;}
-          //to update state
-            for(var i=0; i<data.recs.length; i++){
-            tempProfiles.push(data.recs[i].name);
-            tempMap[data.recs[i].name] = data.recs[i].friends;
-            tempReason[data.recs[i].name] = data.recs[i].reason;
-          }
+            if (!data) { return; }
+            //to update state
+            for (var i = 0; i < data.recs.length; i++) {
+                tempProfiles.push(data.recs[i].name);
+                tempMap[data.recs[i].name] = data.recs[i].friends;
+                tempReason[data.recs[i].name] = data.recs[i].reason;
+            }
 
-          this.setState({
-            reccomendationsProfiles: tempProfiles,
-            mapToProfilesId: tempMap,
-            mapToReason: tempReason
-          });
+            this.setState({
+                reccomendationsProfiles: tempProfiles,
+                mapToProfilesId: tempMap,
+                mapToReason: tempReason
+            });
         });
 
         this.setState({ username: this.username });
@@ -65,30 +72,70 @@ export default class Reccomendations extends React.Component {
                 <div className="topPic">
                 </div>
                 <div className="floater">
-                    <p className="recTitle">{this.state.username}, based on your activity, we reccomend you check out the following people:</p>
-                    <br></br>
-                    <div style={{"marginBottom":"5vw"}} class="container">
-                        <div class="row">
 
-                            {this.state.reccomendationsProfiles.map(profile => (
-                                <div class="col-sm-4">
-                                    <div class="card" style={{ "width": "18rem" }}>
-                                        <div style={{ "height": "10vw" }} class="card-header">
-                                            <span class="nameSpanForCard">{profile.toUpperCase()[0]}</span>
-                                        </div>
-                                        <div class="card-body">
-                                            <p class="card-text"><b>{profile}</b> {this.state.mapToReason[profile]}</p>
-                                            <a style={{"backgroundColor":"orange", "color":"white"}}type="button" class="btn btn-warning" href = {"/profile/" + this.state.mapToProfilesId[profile]} >Go to {profile + "'s"}  profile</a>
+                    <p onClick={this.toggleVisibility} style={{ "marginTop": "7vw" }} className={this.state.onProfile ? 'profPHigh' : 'profP'}>People</p>
+                    <p onClick={this.toggleVisibility} className={this.state.onProfile ? 'compP' : 'compPHigh'}>Places</p>
+
+
+                    <div className={this.state.onProfile ? 'profileArea' : 'hidden'}>
+                        <p className="recTitle">{this.state.username}, based on your activity, we reccomend you check out the following people:</p>
+                        <br></br>
+                        <div style={{ "marginBottom": "5vw" }} class="container">
+                            <div class="row">
+
+                                {this.state.reccomendationsProfiles.map(profile => (
+                                    <div class="col-sm-4">
+                                        <div class="card" style={{ "width": "18rem" }}>
+                                            <div style={{ "height": "10vw" }} class="card-header">
+                                                <span class="nameSpanForCard">{profile.toUpperCase()[0]}</span>
+                                            </div>
+                                            <div class="card-body">
+                                                <p class="card-text"><b>{profile}</b> {this.state.mapToReason[profile]}</p>
+                                                <a style={{ "backgroundColor": "orange", "color": "white" }} type="button" class="btn btn-warning" href={"/profile/" + this.state.mapToProfilesId[profile]} >Go to {profile + "'s"}  profile</a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
 
 
 
 
+                            </div>
                         </div>
                     </div>
+
+                    <div className={this.state.onProfile ? 'hidden' : 'compArea'}>
+
+                        <p className="recTitle">{this.state.username}, based on your activity, we reccomend you check out the following people:</p>
+                        <br></br>
+                        <div style={{ "marginBottom": "5vw" }} class="container">
+                            <div class="row">
+
+                                {this.state.reccomendationsProfiles.map(profile => (
+                                    <div class="col-sm-4">
+                                        <div class="card" style={{ "width": "18rem" }}>
+                                            <div style={{ "height": "10vw" }} class="card-header">
+                                                <span class="nameSpanForCard">{profile.toUpperCase()[0]}</span>
+                                            </div>
+                                            <div class="card-body">
+                                                <p class="card-text"><b>{profile}</b> {this.state.mapToReason[profile]}</p>
+                                                <a style={{ "backgroundColor": "orange", "color": "white" }} type="button" class="btn btn-warning" href={"/profile/" + this.state.mapToProfilesId[profile]} >Go to {profile + "'s"}  profile</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+
+
+
+
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+
                 </div>
                 <span class="nameSpan">{this.state.initial}</span>
 
